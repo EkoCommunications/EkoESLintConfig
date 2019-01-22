1fa0855e155c2b6b3d68cacaf13c17874c1aaf48
JustBeard <hort.yevgen@gmail.com>
2019-01-22 15:16:00 +0700

    add comments

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const ekoRules = [require.resolve('./rules/base'), require.resolve('./rules/import')];
+const ekoRules = [
+  // 'base' eko custom rules
+  require.resolve('./rules/base'),
+  require.resolve('./rules/import'),
+];
 
 module.exports = {
   extends: ['airbnb-base', 'plugin:prettier/recommended', ...ekoRules],
--- react.js
+++ react.js
@@ -1,6 +1,8 @@
 const ekoRules = [
+  // 'base' eko custom rules
   require.resolve('./rules/base'),
   require.resolve('./rules/import'),
+  // 'react' eko custom rules
   require.resolve('./rules/react'),
   require.resolve('./rules/jsx-a11y.js'),
 ];
