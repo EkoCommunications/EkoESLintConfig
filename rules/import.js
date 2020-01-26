d40f09f38eb1fa72a8aa62579d0ef3542f1d3ea4
JustBeard <hort.yevgen@gmail.com>
2020-01-26 22:22:39 +0700

    disable import/no-unused-modules with explanation

--- rules/import.js
+++ rules/import.js
@@ -22,16 +22,30 @@ module.exports = {
     //
     // Team decided to enable now to help catch “dead” components.
     // Expecting extra noise from it during development.
-    // NOTE: rule does not recognize `module.exports` and can generate extra noise.
-    // NOTE2: there is concerns related to performance. eslint-plugin-import team already were
-    // fixing it but I starting to notice more time consuming for running eslint
-    'import/no-unused-modules': [
-      'error',
-      {
-        missingExports: true,
-        unusedExports: true,
-      },
-    ],
+    //
+    // NOTE: Rule does not recognize `module.exports` and can generate extra noise.
+    //       Easiest way to solve - redefine with `ignoreExports: [...folderNames]` option
+    //
+    // NOTE2: There are concerns related to the performance in the community.
+    //        https://github.com/benmosher/eslint-plugin-import/issues/1487
+    //        Tests on WebApp show that performance not affected seriously
+    //        Without the rule: npm run lint  91.68s user 9.66s system 126% cpu 1:20.22 total
+    //        With the rule: npm run lint  95.38s user 9.89s system 125% cpu 1:23.70
+    //
+    // NOTE3: Rule does not handle:
+    //        1) export default from 'Module' - this one is proposal (Stage 1 at the moment).
+    //           We use a lot of it.
+    //        2) export { default } from 'Module'
+    //           link to an issue: https://github.com/benmosher/eslint-plugin-import/issues/1544
+    //
+    // Considering NOTE3 going to disable the rule for now
+    // 'import/no-unused-modules': [
+    //   'error',
+    //   {
+    //     missingExports: true,
+    //     unusedExports: true,
+    //   },
+    // ],
 
     // Ref: https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-useless-path-segments.md
     //
