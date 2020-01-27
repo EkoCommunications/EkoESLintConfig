0fa8828a9367851984242d59c1250a3d05107bcb
JustBeard <hort.yevgen@gmail.com>
2020-01-27 13:14:49 +0700

    revert as rule disabled now

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,1 @@
-const t = [require('./index.js')];
-
-module.exports = {
-  extends: require.resolve('./index.js'),
-  rules: {
-    // Forced to disable rule for eslint-config-eko as it behave weird.
-    // Somehow it reports all exports across package.
-    // Might be removed if we find out that it behave like this everywhere.
-    'import/no-unused-modules': 'off',
-  },
-};
+module.exports = require('./index.js');
